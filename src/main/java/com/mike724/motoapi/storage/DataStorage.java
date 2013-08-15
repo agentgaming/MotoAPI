72aaf5ee21acffe804e43db96b9d6d92fc8f6731
Michael <mike@mike724.com>
2013-08-15 13:04:20 -0400

    Now you can't have access to raw storage. Sorry dakota. :(

--- src/main/java/com/mike724/motoapi/storage/DataStorage.java
+++ src/main/java/com/mike724/motoapi/storage/DataStorage.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.Map;
 
 @SuppressWarnings("unused")
-public class DataStorage {
+class DataStorage {
     final static String TABLE_NAME = "object_storage";
 
     private AmazonDynamoDBClient db;
