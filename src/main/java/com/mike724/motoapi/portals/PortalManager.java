d7bb35169c077dbeda0c09ecf60e08d4afbf4daa
Dakota Baber <dakota@dasigns.net>
2013-08-30 21:31:52 -0400

    Don't call the event if the player is already in a portal and moving

--- src/main/java/com/mike724/motoapi/portals/PortalManager.java
+++ src/main/java/com/mike724/motoapi/portals/PortalManager.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -17,6 +18,8 @@ public class PortalManager implements Listener {
     private HashMap<Integer, ArrayList<Block>> portals = new HashMap<>();
 
     private BlockFace[] facesToCheck = {BlockFace.DOWN, BlockFace.UP, BlockFace.EAST, BlockFace.WEST, BlockFace.NORTH, BlockFace.SOUTH};
+
+    private ArrayList<Player> inPortal = new ArrayList<>();
 
     public PortalManager() {
         portals = new HashMap<>();
@@ -59,14 +62,17 @@ public class PortalManager implements Listener {
     public void onPlayerMove(PlayerMoveEvent e) {
         if (e.isCancelled()) return;
 
-        if (e.getTo().getBlock() == e.getFrom().getBlock()) return;
+        if (e.getTo().getBlock().getX() == e.getFrom().getBlock().getX() &&
+                e.getTo().getBlock().getY() == e.getFrom().getBlock().getY() &&
+                e.getTo().getBlock().getZ() == e.getFrom().getBlock().getZ()) return;
 
         for (Integer i : getPortals().keySet()) {
             ArrayList<Block> portal = getPortals().get(i);
-            if (portal.contains(e.getTo().getBlock())) {
+            if (portal.contains(e.getTo().getBlock()) && !inPortal.contains(e.getPlayer())) {
+                inPortal.add(e.getPlayer());
                 Bukkit.getServer().getPluginManager().callEvent(new PortalEnterEvent(e.getPlayer(), i));
                 break;
-            }
+            } else if (inPortal.contains(e.getPlayer())) inPortal.remove(e.getPlayer());
         }
     }
 }
